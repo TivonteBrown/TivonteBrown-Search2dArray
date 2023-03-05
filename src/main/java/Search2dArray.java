e574d7f19e8f90ec22767f752707b9dc14b611d9
TivonteBrown <TivonteBrown@Gmail.com>
2023-03-05 20:55:05 +0000

    commit 2d array

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,14 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+        for (int[] row : nums) {  // Loop over rows
+            for (int num : row) {  // Loop over columns
+                if (num == target) { 
+                    return true;
+                }
+            }
+        }
+    
         return false;
     }
-}
+    }
